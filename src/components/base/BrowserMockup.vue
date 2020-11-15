9b5926357b6193b567be628724f9994d23c41145
Jess Rascal <me@jessrascal.com>
2020-11-15 11:42:46 +0000

    Created browser mockup component and put in basis for Vue Router page

---
+++ src/components/base/BrowserMockup.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card outlined :min-height="minHeight">
+    <v-system-bar dark>
+      <v-icon small color="error">{{ icons.circle }}</v-icon>
+      <v-icon small color="warning">{{ icons.circle }}</v-icon>
+      <v-icon small color="success">{{ icons.circle }}</v-icon>
+    </v-system-bar>
+    <v-card-title id="address-bar">
+      <v-icon class="action-icon">{{ icons.arrowLeft }}</v-icon>
+      <v-icon class="action-icon">{{ icons.arrowRight }}</v-icon>
+      <v-icon class="action-icon">{{ icons.refresh }}</v-icon>
+      <!-- TODO: bug with min-height on .v-input__slot stops -->
+      <!-- 'height' on field being honoured if less than that (40px dense, 56px normal) -->
+      <v-text-field outlined rounded dense disabled hide-details></v-text-field>
+    </v-card-title>
+    <v-card-text>
+      This is a browser, it just doesn't look like one yet.
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import {
+  mdiCheckboxBlankCircle,
+  mdiArrowLeft,
+  mdiArrowRight,
+  mdiRefresh,
+} from '@mdi/js';
+
+export default {
+  props: {
+    minHeight: {
+      type: String,
+      default: '50vh',
+    },
+  },
+  data() {
+    return {
+      icons: {
+        circle: mdiCheckboxBlankCircle,
+        arrowLeft: mdiArrowLeft,
+        arrowRight: mdiArrowRight,
+        refresh: mdiRefresh,
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#address-bar {
+  background: #363636; // TODO: possible to get from theme variable?
+  padding: 8px;
+
+  // .v-text-field {
+  //   //  without 'outlined'
+  //   margin-top: 0;
+  //   padding-top: 0;
+  //   border: 1px solid #1e1e1e !important;
+  // }
+}
+
+.action-icon {
+  margin-right: 16px;
+}
+</style>
--- src/views/skills/VueRouter.vue
+++ src/views/skills/VueRouter.vue
@@ -1,10 +1,44 @@
 <template>
   <v-container>
     <TheSubtleHeader heading="vue router" />
-    <v-row>
-      <v-col>
-        <p>Vue Router skill page</p>
+    <v-row justify="center">
+      <v-col sm="10" lg="9" xl="8">
+        <div class="text-h4 text-center">
+          <p>
+            I use
+            <ExternalLink url="https://router.vuejs.org/"
+              >Vue Router</ExternalLink
+            >
+            for all of the internal navigation within this app.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col sm="10" lg="9" xl="8">
+        <div class="text-h5 text-center">
+          <p>
+            Here are some <HighlightText>interactive examples</HighlightText> of
+            Vue Router in action.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+    <!-- TODO: tabs -->
+    <v-row justify="center">
+      <v-col sm="10" lg="9" xl="8">
+        <BrowserMockup />
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import BrowserMockup from '@/components/base/BrowserMockup';
+
+export default {
+  components: {
+    BrowserMockup,
+  },
+};
+</script>
